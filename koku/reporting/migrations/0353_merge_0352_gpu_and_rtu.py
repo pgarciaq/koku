ae86532e485d52b917fbf639c3fd958c4ffb9687
Pau Garcia Quiles <pgarciaq@redhat.com>
2026-07-24 02:34:41 +0200

    Add merge migration for conflicting 0352 migrations
    Both 0352_ocpgpuusagelineitem_gpu_pod_utilization and
    0352_rtu_schema_improvements depend on 0351, creating a diamond
    fork that Django cannot resolve automatically. This empty merge
    migration unifies them.

---
+++ koku/reporting/migrations/0353_merge_0352_gpu_and_rtu.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("reporting", "0352_ocpgpuusagelineitem_gpu_pod_utilization"),
+        ("reporting", "0352_rtu_schema_improvements"),
+    ]
+
+    operations = []
